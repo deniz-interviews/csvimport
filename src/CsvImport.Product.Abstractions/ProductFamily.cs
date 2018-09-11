64b8651b9675c45be65640dfa3a87ee7cde04324
Deniz <deniz.tasdogen@outlook.com>
2018-09-11 21:41:13 +0200

    Initial

---
+++ src/CsvImport.Product.Abstractions/ProductFamily.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvImport.Product
+{
+    public class ProductFamily : Entity
+    {
+        public string Key { get; set; }
+        public string Category { get; set; }
+        public virtual IList<Product> Products { get; set; }
+    }
+}
